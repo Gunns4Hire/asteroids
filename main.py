7e18935e9185900b3a94f75cf14a914ff17ae33e
Gunns4Hire <gunnarrachau@gmail.com>
2025-11-27 22:35:10 -0600

    added sprite

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-running = True
-while running:
-    log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill("black")
-
-    pygame.display.flip()
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0.017
+
+    running = True
+    while running:
+        log_state()
+
+        dt = clock.get_time()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+        clock.tick(60)
+        
+        
 
 
 if __name__ == "__main__":
     main()
+
